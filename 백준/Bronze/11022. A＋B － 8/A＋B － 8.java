387db0b5367a33f46187674ab2218e8feeb7001f
dev-s3lim <dev.s3lim@gmail.com>
2025-05-02 17:45:27 +0900

    [Bronze V] Title: A+B - 8, Time: 224 ms, Memory: 18932 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        try (Scanner sc = new Scanner (System.in)){
+            int n = sc.nextInt();
+            int i;
+
+            for (i = 1; i <= n; i++){
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+
+                System.out.println ("Case"+" "+"#"+i+":"+" "+a+" + "+b+" = "+(a+b));
+            }
+        }
+    }
+}

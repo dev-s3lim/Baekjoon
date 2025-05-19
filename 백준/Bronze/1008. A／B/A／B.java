234204ef4b5cb84886b5bcee9a1c30828ab0abb4
dev-s3lim <dev.s3lim@gmail.com>
2025-05-19 19:49:25 +0900

    [Bronze V] Title: A/B, Time: 112 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,10 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-        System.out.println(a / b);
+    public static void main (String [] args) throws IOException {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        String inputs [] = br.readLine().split(" ");
+
+        int a = Integer.parseInt (inputs[0]);
+        int b = Integer.parseInt (inputs[1]);
+
+        System.out.printf ("%.9f\n", (double) a / b);
     }
 }

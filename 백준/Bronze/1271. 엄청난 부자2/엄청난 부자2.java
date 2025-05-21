cf1eabb3400337bbd9999c7d5bd5e8400e625743
dev-s3lim <dev.s3lim@gmail.com>
2025-05-21 18:56:43 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -1,24 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input [] = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BigInteger n = new BigInteger(input[0]);
-        BigInteger m = new BigInteger(input[1]);
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
 
-        if (m.equals(BigInteger.ZERO)){
-            System.out.println ("0으로는 나눌 수 없습니다.");
+        if (m.equals(BigInteger.ZERO)) {
+            System.out.println("0으로는 나눌 수 없습니다.");
+            return;
         }
-        else {
-            BigInteger a = n.divide(m);
-            BigInteger b = n.remainder(m);
 
-            System.out.println(a + "\n" + b);
-        }
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
     }
 }

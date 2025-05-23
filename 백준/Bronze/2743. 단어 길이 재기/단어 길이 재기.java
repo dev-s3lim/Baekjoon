bbed75489b1628a52e81732d61babf0343bee947
dev-s3lim <dev.s3lim@gmail.com>
2025-05-23 20:29:55 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,13 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main (String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            String s = scanner.nextLine(); // 단어 입력 받기
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
 
-            int l = s.length(); // 단어의 길이
-
-            System.out.println (l); // 정수로 변환된 단어 길이 l 출력
-        }
+        System.out.println(word.length());
     }
 }

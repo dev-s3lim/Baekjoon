52788058dc5b1fb232eb80a253af5619dc650e8c
dev-s3lim <dev.s3lim@gmail.com>
2025-05-23 21:22:11 +0900

    [Bronze III] Title: 최댓값, Time: 132 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,27 +1,25 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main (String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            int i;
-            int p = 0;
-            int a[] = new int[9];
+    public static void main (String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int i = 0; // 숫자를 입력받을 횟수 초기화
+        int index = 1; // 몇번째인지 판별해줄 변수 (항상 첫번째부터 세기때문에 1로 초기화)
+        int max = i; // 가장 처음부터 입력받는 숫자를 임시 변수인 max에 대입 후 다음 번 입력받는 수와 비교할 것이기 때문에 i로 초기화
+        
+        for (i = 0; i < 9; i++){ 
+            String input = br.readLine(); // 숫자를 총 9번 입력 후...
+            int n = Integer.parseInt(input); // 정수로 변환하고...
 
-            for (i = 0; i < 9; i++) {
-                a[i] = sc.nextInt();
+            if (n > max){ // 정수로 변환한 입력된 숫자가 max보다 크다면...
+                max = n; // 그 숫자가 곧 max, 가장 큰 수이다.
+                index = i + 1; // 0번째부터 for 문이 작동하므로 +1
             }
 
-            int max = a[0];
-
-            for (i = 0; i < 9; i++) {
-                if (a[i] > max) {
-                    max = a[i];
-                    p = i;
-                }
-            }
-
-            System.out.println(max);
-            System.out.println(p+1);
         }
+
+        System.out.println(max);
+        System.out.println(index);
+
     }
 }

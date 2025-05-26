9819333dabfc165ccbe9701f6d7abf468aaa37f7
dev-s3lim <dev.s3lim@gmail.com>
2025-05-26 13:50:11 +0900

    [Bronze II] Title: 문자열 반복, Time: 112 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,25 +1,23 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            int t = sc.nextInt();
-            sc.nextLine();
+    public static void main (String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        char c = 0;
 
-            for (int i = 0; i < t; i++) {
-                int r = sc.nextInt();
-                String s = sc.next();
+        for (int i = 0 ; i < t ; i++){
+            String s[] = br.readLine().split(" ");
+            int r = Integer.parseInt(s[0]);
+            String line = s[1];
 
-                String result = "";
-                for (int j = 0; j < s.length(); j++) {
-                    char c = s.charAt(j);
-                    for (int k = 0; k < r; k++) {
-                        result += c;
-                    }
+            for (int j = 0 ; j < line.length() ; j++){
+                c = line.charAt(j);
+                for (int k = 0 ; k < r; k++){
+                    System.out.print(c);
                 }
-
-                System.out.println(result);
             }
+            System.out.print('\n');
         }
     }
 }

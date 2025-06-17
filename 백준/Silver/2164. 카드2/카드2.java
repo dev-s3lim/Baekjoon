1c9a065ef6f6796d291187487e3437dd8678f983
dev-s3lim <dev.s3lim@gmail.com>
2025-06-17 09:01:40 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 45568 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,21 @@
+ import java.io.*;
+ import java.util.*;
+
+ public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> cards = new LinkedList<>();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1 ; i < n+1; i++){
+            cards.offer(i);
+        }
+
+        while(cards.size() > 1){
+            cards.pollFirst();
+            cards.offerLast(cards.pollFirst());
+        }
+
+        System.out.println(cards.peek());
+    }
+ }

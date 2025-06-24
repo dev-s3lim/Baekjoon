b5f5db00ac2348c7d3283b0972f184f9ecca903e
dev-s3lim <dev.s3lim@gmail.com>
2025-06-24 11:39:21 +0900

    [Silver V] Title: 단어 정렬, Time: 372 ms, Memory: 25672 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -2,29 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main (String [] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<String> list = new ArrayList<>();
+        Set<String> mySet = new HashSet<>();
         int n = Integer.parseInt(br.readLine());
 
-        Set<String> remover = new HashSet<>();
-
-        for (int i = 0 ; i < n ; i++){
-            remover.add(br.readLine());
+        for (int i = 0; i < n; i++){
+            String v = br.readLine();
+            mySet.add(v);
         }
 
-        List<String> words = new ArrayList<>(remover);
+        for(String s : mySet){
+            list.add(s);
+        }
 
-        Collections.sort(words, (a, b) -> {
-            if (a.length() == b.length()){
-                return a.compareTo(b);
-            }
-            else {
-                return Integer.compare(a.length(), b.length());
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
             }
         });
 
-        for (String word : words){
-            System.out.println(word);
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String a : list){
+            sb.append(a).append("\n");
         }
+
+        System.out.print(sb);
     }
 }

638054a69a418d9073e6388de373c4da4e10a3c5
dev-s3lim <dev.s3lim@gmail.com>
2025-06-30 13:40:02 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.06 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        Queue<Integer> pq = new PriorityQueue<>();
+        int[] answer = new int[score.length];
+        
+        for (int i = 0; i < score.length; i++) {
+            pq.add(score[i]);
+
+            if (pq.size() > k) {
+                answer[i] = pq.poll();
+            }
+
+            answer[i] = pq.peek();
+            
+        }
+        return answer;
+    }
+}

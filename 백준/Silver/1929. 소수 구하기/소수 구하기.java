267a15a8ffbe7770e512d7cc7b7a2ab40d345e58
dev-s3lim <dev.s3lim@gmail.com>
2025-07-04 11:07:35 +0900

    [Silver III] Title: 소수 구하기, Time: 980 ms, Memory: 26764 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int m = Integer.parseInt(input[0]);
+        int n = Integer.parseInt(input[1]);
+
+        for (int i = m; i <= n; i++) {
+            boolean isPrime = true;
+            if (i < 2) {
+                continue;
+            }
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+    }
+}
